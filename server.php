9b611fa02d42aacdb1eaae1dae8f6029f5cc809c
rahulkumarsharma7711 <72204639+rahulkumarsharma7711@users.noreply.github.com>
2020-10-01 19:34:08 +0545

    Update server.php

--- server.php
+++ server.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Laravel - A PHP Framework For Web Artisans
+ * Laravel - A PHP Framework For Web Artificer
  *
  * @package  Laravel
  * @author   Taylor Otwell <taylor@laravel.com>
